6a5015f74520895f84cdbc1c0db75460dd6e40da
Anton.Mukhortyh <Anton.Mukhortyh@waveaccess.ru>
2018-03-13 06:38:50 +0300

    init

---
+++ GridFiltering/FilterExtentions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GridFiltering
+{
+    public static class FilterExtentions
+    {
+        public static IQueryable<T> ApplyFilter<T>(this IQueryable<T> src, FilterModel<T> filterModel) where T:class
+        {
+            foreach (var filter in filterModel.Filters)
+            {
+                src = src.Where(filter);
+            }
+
+            return src;
+        }
+
+        public static IQueryable<T> ApplySorting<T>(this IQueryable<T> src, FilterModel<T> filterModel) where T : class
+        {
+            var sortExpression = filterModel.Sorter;
+            if (sortExpression == null) return src;
+
+            return filterModel.IsAscSort ? src.OrderBy(sortExpression) : src.OrderByDescending(sortExpression);
+        }
+
+        public static IQueryable<T> ApplyPaging<T>(this IQueryable<T> src, FilterModel<T> filterModel) where T : class
+        {
+            return src.Skip((filterModel.PageNumber - 1) * filterModel.PageSize).Take(filterModel.PageSize);
+        }
+    }
+}
---
+++ GridFiltering/FilterModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace GridFiltering
+{
+    public abstract class FilterModel<T> where T : class
+    {
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+
+        public bool IsAscSort { get; set; }
+
+        public string SortField { get; set; }
+
+        //[JsonIgnore]
+        protected abstract Dictionary<string, Expression<Func<T, object>>> FieldsMapping { get; }
+
+        //[JsonIgnore]
+        public virtual Expression<Func<T, object>> Sorter => !string.IsNullOrEmpty(SortField) && FieldsMapping.ContainsKey(SortField.ToLower()) 
+            ? FieldsMapping[SortField.ToLower()] 
+            : null;
+
+        //[JsonIgnore]
+        public abstract IEnumerable<Expression<Func<T, bool>>> Filters { get; }
+    }
+}
